2f53a035ec1176d6fd4247d37dbe13c678f70988
Viccodes <ayeniv69@gmail.com>
2025-06-06 15:18:41 +0100

    generated my components and defined there routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { ProjectsComponent } from './components/pages/projects/projects.component';
+import { ServicesComponent } from './components/pages/services/services.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }, {
+    path: 'home', 
+    component: HomeComponent
+  }, {
+    path: 'about', 
+    component: AboutComponent
+  }, {
+    path: 'projects',
+    component: ProjectsComponent
+  }, {
+    path: 'services',
+    component: ServicesComponent
+  }
+];
---
+++ src/app/components/shared/right-sidebar/right-sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right-sidebar',
+  imports: [],
+  templateUrl: './right-sidebar.component.html',
+  styleUrl: './right-sidebar.component.scss'
+})
+export class RightSidebarComponent {
+
+}
