e5a3940d58d017b3904e4e5dad1f3ebda19f7f7b
Viccodes <ayeniv69@gmail.com>
2025-06-07 12:24:35 +0100

    finished the projects page

--- src/app/components/pages/projects/projects.component.ts
+++ src/app/components/pages/projects/projects.component.ts
@@ -7,5 +7,70 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-
+  projects = [
+    {
+      image: 'assets/projects/img1.png',
+      title: 'UI/UX DESIGN',
+      headerText: 'Login Form',
+      text: 'Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.',
+      href: 'https://github.com/vay-dev'
+    },
+    {
+      image: 'assets/projects/img2.png',
+      title: 'UI/UX DESIGN',
+      headerText: 'Fashion Designer Portfolio',
+      text: 'Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.',
+      href: 'https://github.com/vay-dev'
+    },
+    {
+      image: 'assets/projects/img3.png',
+      title: 'UI/UX DESIGN',
+      headerText: 'Login Form',
+      text: 'Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.',
+      href: 'https://github.com/vay-dev'
+    },
+    {
+      image: 'assets/projects/img4.png',
+      title: 'UI/UX DESIGN',
+      headerText: 'Shopping Website',
+      text: 'Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.',
+      href: 'https://github.com/vay-dev'
+    },
+    {
+      image: 'assets/projects/img5.png',
+      title: 'UI/UX DESIGN',
+      headerText: 'Kalban Inventory',
+      text: 'Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.',
+      href: 'https://github.com/vay-dev'
+    },
+    {
+      image: 'assets/projects/img6.png',
+      title: 'UI/UX DESIGN',
+      headerText: 'Model Website',
+      text: 'Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.',
+      href: 'https://github.com/vay-dev'
+    },/*
+    {
+      image: 'assets/projects/img7.png',
+      title: 'UI/UX DESIGN',
+      headerText: 'Gift Card Shipping',
+      text: 'Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.',
+      href: 'https://github.com/vay-dev'
+    },
+    {
+      image: 'assets/projects/img8.png',
+      title: 'UI/UX DESIGN',
+      headerText: 'E-commmerse Website',
+      text: 'Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.',
+      href: 'https://github.com/vay-dev'
+    },
+    {
+      image: 'assets/projects/img9.png',
+      title: 'UI/UX DESIGN',
+      headerText: 'Creative',
+      text: 'Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.',
+      href: 'https://github.com/vay-dev'
+    }
+    */
+  ]
 }
