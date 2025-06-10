c7e42e0e783901fc52ea533f3c9b9615f172eeca
Viccodes <ayeniv69@gmail.com>
2025-06-10 01:17:36 +0100

    finished the about page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { MainComponent } from './components/main/main.component';
-import { FooterComponent } from './components/shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, MainComponent, FooterComponent],
+  imports: [NavbarComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/pages/about/about.component.ts
+++ src/app/components/pages/about/about.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { NgbCarousel, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  standalone: true,
+  imports: [NgIf, NgbCarouselModule],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
 export class AboutComponent {
+  testimonials = [
+    {
+      text: 'Amazing experience!',
+      name: 'Esther Howard',
+      position: 'Managing Director, ABC Company',
+    },
+    {
+      text: 'Top-notch quality.',
+      name: 'Jacob Jones',
+      position: 'CTO, XYZ Inc.',
+    },
+    {
+      text: 'Very good design.',
+      name: 'Jhon Doe',
+      position: 'KLE, WER Ltd.',
+    },
+  ];
 
+  currentSlide: number = 0;
+
+  @ViewChild('carouselRef', { static: false }) carouselRef!: NgbCarousel;
+
+  onSlide(slideEvent: any) {
+    this.currentSlide = Number(slideEvent.current);
+  }
+
+  goToNext() {
+    this.carouselRef.next();
+  }
+
+  goToPrev() {
+    this.carouselRef.prev();
+  }
 }
